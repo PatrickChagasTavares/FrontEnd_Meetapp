432d53560b96e88a49fdd06027a0e48d7410e897
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-08-18 16:40:03 -0300

    create: adding and configuring redux, redux-saga and axios

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
 
+import { Provider } from 'react-redux';
 import Routes from './routes';
 import history from './services/history';
+
+import store from './store';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-      <GlobalStyle />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
   );
 }
 

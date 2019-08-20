eb42093ef1a9ba76389a74544dc872a5562273cd
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-08-20 01:15:17 -0300

    update: adding method to send authorization with token

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import api from '~/services/api';
 // import { Container } from './styles';
 
 export default function Dashboard() {
+  api.get('meetups');
   return (
     <div>
       <h1>Page Dashboard</h1>

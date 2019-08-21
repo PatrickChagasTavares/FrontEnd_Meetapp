6824e144ebe31f59ba83c454d8ecc0de02ff1ab5
Patrick Chagas <patrick@pesqueiracontabilidade.com.br>
2019-08-21 16:33:37 -0300

    create: adding component to notification and your styles

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { MdNotifications } from 'react-icons/md';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Scroll,
+  Notification,
+} from './styles';
+
+export default function Notifications() {
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#f94d6a" />
+      </Badge>
+
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>Você possui um novo agendamento para amanhâ</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhâ</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhâ</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhâ</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhâ</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agendamento para amanhâ</p>
+            <time>há 2 dias</time>
+            <button type="button">Marcar como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
+}
---
+++ src/components/Notifications/styles.js
@@ -0,0 +1,97 @@
+import styled, { css } from 'styled-components';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { lighten } from 'polished';
+
+export const Container = styled.div`
+  position: relative;
+`;
+
+export const Badge = styled.button`
+  background: none;
+  border: 0;
+  position: relative;
+
+  ${props =>
+    props.hasUnread &&
+    css`
+      &::after {
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 8px;
+        height: 8px;
+        background: #fff;
+        content: '';
+        border-radius: 50%;
+      }
+    `}
+`;
+
+export const NotificationList = styled.div`
+  position: absolute;
+  width: 260px;
+  left: calc(50% - 130px);
+  top: calc(100% + 30px);
+  background: rgba(249, 77, 106, 0.5);
+  border-radius: 4px;
+  padding: 15px 5px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: calc(50% - 20px);
+    top: -20px;
+    width: 0;
+    height: 0;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 20px solid rgba(249, 77, 106, 0.5);
+  }
+`;
+
+export const Scroll = styled(PerfectScrollbar)`
+  max-height: 260px;
+  padding: 5px 15px;
+`;
+
+export const Notification = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+
+  & + div {
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid rgba(0, 0, 0);
+  }
+
+  p {
+    font-size: 13px;
+    line-height: 18px;
+  }
+  time {
+    font-size: 12px;
+    opacity: 0.6;
+  }
+
+  button {
+    font-size: 12px;
+    border: 0;
+    background: none;
+    color: ${lighten(0.2, '#f94d6a')};
+    padding: 0 5px;
+    margin: 0 5px;
+    border-left: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  ${props =>
+    props.unread &&
+    css`
+      &::after {
+        content: '';
+        display: inline-block;
+        width: 8px;
+        height: 8px;
+        background: #fff;
+        border-radius: 50%;
+      }
+    `}
+`;
